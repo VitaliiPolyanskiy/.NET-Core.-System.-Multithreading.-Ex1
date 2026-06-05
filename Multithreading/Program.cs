2b090109373a8ea981f1fe601fe82bad62e1391c
Admin <polyanskiy@itstep.academy>
2026-06-05 21:47:22 +0300

    Changing the version to .NET 10 Add comments in Ukrainian

--- Multithreading/Program.cs
+++ Multithreading/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Threading;
 
 public class Multithreading
@@ -7,10 +8,10 @@ public class Multithreading
     {
         for (int i = 0; i < 20; i++)
         {
-            Console.WriteLine("Работает первый поток!");
+            Console.WriteLine("Працює перший потік!");
             Thread.Sleep(100);
         }
-        Console.WriteLine("Завершает работу первый поток!");
+        Console.WriteLine("Завершує роботу перший потік!");
     }
 
     public static void ThreadParam(object obj)
@@ -20,72 +21,75 @@ public class Multithreading
 
         for (int i = 0; i < 10; i++)
         {
-            Console.WriteLine("Работает " + t.Name + " поток!");
+            Console.WriteLine("Працює " + t.Name + " потік!");
             Thread.Sleep(delay);
         }
-        Console.WriteLine("Завершает работу " + t.Name + " поток!");
+        Console.WriteLine("Завершує роботу " + t.Name + " потік!");
 
-        Console.WriteLine("Имя потока: {0}", t.Name);
-        Console.WriteLine("Запущен ли поток: {0}", t.IsAlive);
-        Console.WriteLine("Приоритет потока: {0}", t.Priority);
-        Console.WriteLine("Статус потока: {0}", t.ThreadState);
+        Console.WriteLine("Ім'я потоку: {0}", t.Name);
+        Console.WriteLine("Чи запущений потік: {0}", t.IsAlive);
+        Console.WriteLine("Пріоритет потоку: {0}", t.Priority);
+        Console.WriteLine("Статус потоку: {0}", t.ThreadState);
     }
 
     public static void Main()
     {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+
         Thread t = Thread.CurrentThread;
 
-        Console.WriteLine("Имя потока: {0}", t.Name);
+        Console.WriteLine("Ім'я потоку: {0}", t.Name);
         t.Name = "Метод Main";
-        Console.WriteLine("Имя потока: {0}", t.Name);
+        Console.WriteLine("Ім'я потоку: {0}", t.Name);
 
-        Console.WriteLine("Запущен ли поток: {0}", t.IsAlive);
-        Console.WriteLine("Приоритет потока: {0}", t.Priority);
-        Console.WriteLine("Статус потока: {0}", t.ThreadState);
+        Console.WriteLine("Чи запущений потік: {0}", t.IsAlive);
+        Console.WriteLine("Пріоритет потоку: {0}", t.Priority);
+        Console.WriteLine("Статус потоку: {0}", t.ThreadState);
 
         Thread th = new Thread(new ThreadStart(MyThread));
         th.IsBackground = true;
-        Console.WriteLine("Имя потока: {0}", th.Name);
-        Console.WriteLine("Запущен ли поток: {0}", th.IsAlive);
-        Console.WriteLine("Статус потока: {0}", th.ThreadState);
+        Console.WriteLine("Ім'я потоку: {0}", th.Name);
+        Console.WriteLine("Чи запущений потік: {0}", th.IsAlive);
+        Console.WriteLine("Статус потоку: {0}", th.ThreadState);
         th.Start();
 
         Thread th1 = new Thread(new ParameterizedThreadStart(ThreadParam));
         th1.IsBackground = true;
-        th1.Name = "второй";
+        th1.Name = "другий";
         th1.Start(200);
 
         Thread th2 = new Thread(new ParameterizedThreadStart(ThreadParam));
         th2.IsBackground = true;
-        th2.Name = "третий";
+        th2.Name = "третій";
         th2.Start(80);
 
         Console.ReadKey();
 
-        Console.WriteLine("Имя потока: {0}", th.Name);
-        Console.WriteLine("Запущен ли поток: {0}", th.IsAlive);
-        Console.WriteLine("Статус потока: {0}", th.ThreadState);
+        Console.WriteLine("Ім'я потоку: {0}", th.Name);
+        Console.WriteLine("Чи запущений потік: {0}", th.IsAlive);
+        Console.WriteLine("Статус потоку: {0}", th.ThreadState);
     }
 
     /*
-       Поток является либо фоновым, либо основным (по умолчанию).
-       Они отличаются по принципу действия. 
-       Если работает фоновый поток и происходит закрытие приложения, поток также выгружается из памяти. 
-       Основной же поток при закрытии приложения останется в памяти.
-       Если все основные потоки, принадлежащие процессу, завершились, общеязыковая среда выполнения завершает процесс, 
-       вызывая метод Abort для всех фоновых потоков, которые еще действуют.
+       Потік є або фоновим, або основним (за замовчуванням).
+       Вони відрізняються за принципом дії. 
+       Якщо працює фоновий потік і відбувається закриття додатка, потік також вивантажується з пам'яті. 
+       Основний потік при закритті додатка залишиться в пам'яті.
+       Якщо всі основні потоки, що належать процесу, завершилися, загальномовне середовище виконання завершує процес, 
+       викликаючи метод Abort для всіх фонових потоків, які ще діють.
         
        public Thread(
-       ThreadStart start – делегат, который указывает на потоковую функцию.
+       ThreadStart start – делегат, який вказує на потокову функцію.
        );
        public delegate void ThreadStart();
 
-       public Thread(ParameterizedThreadStart start - делегат, который ссылается на потоковую функцию с 
+       public Thread(ParameterizedThreadStart start - делегат, який посилається на потокову функцію з 
                            параметром. 
        );
 
        public delegate void ParameterizedThreadStart(
-       object obj – объект, который будет передаваться в потоковую функцию.
+       object obj – об'єкт, який буде передаватися в потокову функцію.
        );  
-       */
+    */
 }
